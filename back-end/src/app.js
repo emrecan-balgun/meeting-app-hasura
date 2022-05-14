2f2e01528f1aeaac1ea637d57b8d6efa5832dfec
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-14 18:44:42 +0300

    add sucrase and refactor package json and update app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,12 +1,10 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 app.use(express.json());
 
 app.post('/register', (req, res) => {
     const input = req.body.input.data;
-
-    console.log("input", input);
 
     res.json({
         accessToken: 'accessToken'
