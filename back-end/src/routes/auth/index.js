42b3d21bc1756b3979b292223f643f5095971e36
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-14 19:02:05 +0300

    refactor app.js and add error handling with boom

--- back-end/src/routes/auth/index.js
+++ back-end/src/routes/auth/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import Boom from 'boom';
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
+router.post('/register', (req, res, next) => {
     const input = req.body.input.data;
+
+    if(!input.email || !input.password) {
+        return next(Boom.badRequest('Email and password are required'));
+    }
     res.json({ accessToken: 'accessToken' });
 })
 
