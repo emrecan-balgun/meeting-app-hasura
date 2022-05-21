f1a1f13a03499771a294e8f250d960b4ce8666a8
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-21 18:58:32 +0300

    add node mailer and update index.js

--- back-end/src/routes/webhooks/index.js
+++ back-end/src/routes/webhooks/index.js
@@ -7,22 +7,43 @@ import { GET_MEETING_PARTICIPANTS } from './queries';
 
 const router = express.Router();
 
-const transporter = nodemailer.createTransport({
-    service: "google",
+const smptpConfig = {
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD,
-    }
-})
+        pass: process.env.GMAIL_PASSWORD
+    },
+};
+
+const transporter = nodemailer.createTransport(smptpConfig);
 
 router.post('/meeting_created', async (req, res, next) => {
     const meeting = req.body.event.data.new;
     
-    const data = await Hasura.request(GET_MEETING_PARTICIPANTS, {
-        meeting_id: meeting.id,
+    const { meetings_by_pk } = await Hasura.request(GET_MEETING_PARTICIPANTS, {
+        id: meeting.id,
     });
 
+    const title = meeting.title;
+    const { email, fullName } = meetings_by_pk.user;
+    const participants = meetings_by_pk.participants.map(({ user }) => (user.email).toString());
 
+    const mailOptions = {
+        from: process.env.GMAIL_USER,
+        to: participants,
+        subject: `${fullName} invited you to a meeting!`,
+        text: `${fullName} invited you to a meeting!\n\n${title}`,
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            throw new Error(err);
+        } else {
+            return res.json({ info });
+        }
+    });
 
 });
 
--- back-end/src/routes/webhooks/queries.js
+++ back-end/src/routes/webhooks/queries.js
@@ -1,7 +1,6 @@
 export const GET_MEETING_PARTICIPANTS = `
 query getParticipants($id: Int!){
     meetings_by_pk(id: $id){
-      title
       user{
         email
         fullName
