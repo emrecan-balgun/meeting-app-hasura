9020e7cd9eccc6f54fb9a73d13fbcd16860baace
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-23 16:17:34 +0300

    add get meeting participants reminder query

--- back-end/src/routes/webhooks/queries.js
+++ back-end/src/routes/webhooks/queries.js
@@ -15,3 +15,28 @@ query getParticipants($id: Int!){
     }
   }
 `;
+
+export const GET_MEETING_PARTICIPANTS_REMINDER_QUERY = `
+query getParticipants($id: Int!){
+  meetings_by_pk(id: $id){
+    title
+    meeting_date
+    user{
+      email
+      fullName
+    }
+    participants(
+      where: {
+        is_approved:{
+          _eq: true
+        }
+      }
+    ){
+      user{
+        fullName
+        email
+      }
+    }
+  }
+}
+`;
